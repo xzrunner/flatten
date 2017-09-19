4ac54d8826cb61b3f1567f5ee15e8ea3965e9f8d
zz <zhuguang@ejoy.com>
2017-09-19 20:21:06 +0800

    [ADDED] use shared ptr

--- include/flatten/BuildFlattenVisitor.h
+++ include/flatten/BuildFlattenVisitor.h
@@ -5,6 +5,7 @@
 #include <sprite2/SpriteVisitor.h>
 
 #include <vector>
+#include <memory>
 
 namespace ft
 {
@@ -14,7 +15,7 @@ class Flatten;
 class BuildFlattenVisitor : public s2::SpriteVisitor
 {
 public:
-	BuildFlattenVisitor(Flatten& flatten);
+	BuildFlattenVisitor(std::shared_ptr<ft::Flatten> flatten);
 
 	virtual s2::VisitResult Visit(const s2::Sprite* spr, const s2::SprVisitorParams& params);
 
@@ -22,7 +23,7 @@ public:
 	virtual s2::VisitResult VisitChildrenEnd(const s2::Sprite* spr, const s2::SprVisitorParams& params);
 
 private:
-	Flatten& m_flatten;
+	std::shared_ptr<ft::Flatten> m_flatten;
 
 	std::vector<int> m_curr_path;
 
--- source/BuildFlattenVisitor.cpp
+++ source/BuildFlattenVisitor.cpp
@@ -2,26 +2,32 @@
 #include "flatten/Flatten.h"
 
 #include <sprite2/SprVisitorParams.h>
+#include <sprite2/S2_Actor.h>
 
 #include <assert.h>
 
 namespace ft
 {
 
-BuildFlattenVisitor::BuildFlattenVisitor(Flatten& flatten)
+BuildFlattenVisitor::BuildFlattenVisitor(std::shared_ptr<ft::Flatten> flatten)
 	: m_flatten(flatten)
 {
 }
 
 s2::VisitResult BuildFlattenVisitor::Visit(const s2::Sprite* spr, const s2::SprVisitorParams& params)
 {
-	assert(m_flatten.m_nodes_cap > 0 && m_flatten.m_nodes_sz < m_flatten.m_nodes_cap);
-	if (params.actor) {
-		m_flatten.m_nodes[m_flatten.m_nodes_sz++].Init(params.actor);
-	} else {
-		m_flatten.m_nodes[m_flatten.m_nodes_sz++].Init(spr);
+	assert(m_flatten->m_nodes_cap > 0 && m_flatten->m_nodes_sz < m_flatten->m_nodes_cap);
+	if (params.actor) 
+	{
+		int pos = m_flatten->m_nodes_sz;
+		m_flatten->m_nodes[m_flatten->m_nodes_sz++].Init(params.actor);
+		const_cast<s2::Actor*>(params.actor)->SetFlatten(m_flatten, pos);
+	} 
+	else 
+	{
+		m_flatten->m_nodes[m_flatten->m_nodes_sz++].Init(spr);
 	}
-	Flatten::Node& node = m_flatten.m_nodes[m_flatten.m_nodes_sz - 1];
+	Flatten::Node& node = m_flatten->m_nodes[m_flatten->m_nodes_sz - 1];
 	if (m_curr_path.empty()) {
 		node.m_parent = -1;
 	} else {
@@ -32,7 +38,7 @@ s2::VisitResult BuildFlattenVisitor::Visit(const s2::Sprite* spr, const s2::SprV
 
 s2::VisitResult BuildFlattenVisitor::VisitChildrenBegin(const s2::Sprite* spr, const s2::SprVisitorParams& params)
 {
-	m_curr_path.push_back(m_flatten.m_nodes_sz - 1);
+	m_curr_path.push_back(m_flatten->m_nodes_sz - 1);
 	return s2::VISIT_OVER;
 }
 
@@ -40,13 +46,13 @@ s2::VisitResult BuildFlattenVisitor::VisitChildrenEnd(const s2::Sprite* spr, con
 {
 	int curr = m_curr_path.back();
 	m_curr_path.pop_back();
-	int tot = m_flatten.m_nodes_sz;
+	int tot = m_flatten->m_nodes_sz;
 	assert(tot >= curr);
-	Flatten::Node& node = m_flatten.m_nodes[curr];
+	Flatten::Node& node = m_flatten->m_nodes[curr];
 	node.m_count = tot - curr;
 	node.m_layer = m_curr_path.size();
-	if (node.m_layer > m_flatten.m_max_layer) {
-		m_flatten.m_max_layer = node.m_layer;
+	if (node.m_layer > m_flatten->m_max_layer) {
+		m_flatten->m_max_layer = node.m_layer;
 	}
 	return s2::VISIT_OVER;
 }
